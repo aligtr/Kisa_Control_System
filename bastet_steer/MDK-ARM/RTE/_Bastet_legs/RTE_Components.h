be32d81bc316feccb6514dbff3c899283580205e
aligtr <37243243+aligtr@users.noreply.github.com>
2022-01-18 22:16:48 +0300

    Add files via upload

---
+++ bastet_steer/MDK-ARM/RTE/_Bastet_legs/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Bastet_legs' 
+ * Target:  'Bastet_legs' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
